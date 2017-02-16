4af93d0117ed1161a107df85cb8b7e84fd5300de
Madays <maday.choque@gmail.com>
2017-02-16 18:00:51 -0500

    test20

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 function logIn(evt){
-    /*if(localStorage.length==0){
-        alert('Sign Up');        
-    }else{
-        location.href='app.html';
-    }*/
+
     evt.preventDefault();
     console.log(localStorage.getItem('flag'));
     if(localStorage.getItem('flag')!=null){
