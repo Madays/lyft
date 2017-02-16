129f4e537948823be87dc777bbb1da93f4c6f654
Madays <maday.choque@gmail.com>
2017-02-16 17:58:01 -0500

    la etiqueta de log in es un a y este tiene naturaleza de enlace pero se cancela el evento href porque ahi yo le cargare un html

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-function logIn(){
-    if(localStorage.length==0){
+function logIn(evt){
+    /*if(localStorage.length==0){
         alert('Sign Up');        
     }else{
         location.href='app.html';
+    }*/
+    evt.preventDefault();
+    console.log(localStorage.getItem('flag'));
+    if(localStorage.getItem('flag')!=null){
+        //alert('si se puede');
+        location.href='app.html';
+    }else{
+        alert('Sign Up');
     }
 }
