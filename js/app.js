353d317646e3aa4c7094d7520dc999e33cf63bfa
Madays <maday.choque@gmail.com>
2017-02-16 13:59:58 -0500

    posicion dinamica

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function initMap() {
+/*function initMap() {
   var myLatLng = [
       {lat: -16.1511485, lng: -71.531948},{lat: -15.4521485, lng: -71.532948},{lat: -16.4531485, lng: -74.533948},{lat: -15.4541485, lng: -73.534948},{lat: -16.4571485, lng: -71.532948}];
   map = new google.maps.Map(document.getElementById('map'), {
@@ -15,4 +15,64 @@ function initMap() {
         });
     }
     
-};
+};*/
+
+
+
+/*function initMap() {
+  getLocation();            
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    var myLatLng = {lat:position.coords.latitude,lon:position.coords.longitude};
+    console.log(myLatLng);
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: myLatLng.lat, lng: myLatLng.lon},
+    zoom: 8
+  });
+    
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+        map: map,
+        position: {lat: myLatLng.lat, lng: myLatLng.lon},
+        title: 'Hello World!',
+        icon:"img/taxi2.png"
+    });
+}
+*/
+function initMap() {
+  getLocation();            
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    var myLatLng = {lat:position.coords.latitude,lon:position.coords.longitude};
+    console.log(myLatLng);
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: myLatLng.lat, lng: myLatLng.lon},
+    zoom: 8
+  });
+    
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+        map: map,
+        position: {lat: myLatLng.lat, lng: myLatLng.lon},
+        title: 'Hello World!',
+        icon:"img/taxi2.png"
+    });
+}
