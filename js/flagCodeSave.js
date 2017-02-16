bdd5fcc9d634a6164aa0aff6a88f764f34de8b31
Madays <maday.choque@gmail.com>
2017-02-16 11:54:29 -0500

    obtener codigo de pais del local storage

--- js/flagCodeSave.js
+++ js/flagCodeSave.js
@@ -7,4 +7,5 @@ for(var k=0;k<13;k++){
 function saveFlagCodeCountry(evt){
     console.log(evt.target.parentNode.firstChild.src);
     localStorage.setItem('flag',evt.target.parentNode.firstChild.src);       
+    console.log(evt.target.parentNode.firstChild.nextElementSibling.nextElementSibling.textContent); localStorage.setItem('codeCountry',evt.target.parentNode.firstChild.nextElementSibling.nextElementSibling.textContent);       
 }
--- js/selectCountry.js
+++ js/selectCountry.js
@@ -5,6 +5,7 @@ for(var i=0;i<infoCountry.length;i++){
     var htmlLiCountry= '<li id="country" class="country"><a href="" style="text-decoration: none;" id="link">'+        
             '<img src='+ infoCountry[i].srcImg +' alt="" style="padding-right:10px;" id="flag-country">'+
             '<span>'+infoCountry[i].name+'</span>'+
+            '<span id="code-country">'+infoCountry[i].phone_code+'</span>'+
                 '<span style="float:right;margin-top:-5px;padding-right: 4px;">'+alphabet[2*i]+'<br>'+alphabet[2*i+1]+'</span>'+
                 '<hr>'+
           '</a></li>';
